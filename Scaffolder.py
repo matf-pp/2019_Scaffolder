0c3b4e2b7e4df289b1c2657b7c313db8ca46c3ad
Lamvorn <47858044+Lamvorn@users.noreply.github.com>
2019-03-16 01:01:53 +0100

    Add files via upload

--- Scaffolder.py
+++ Scaffolder.py
@@ -19,8 +19,9 @@ def ispisi_poruku(poruka , boja , y_pomeraj = 0, vel_font=25):
     TextRect.center = (255,500/2 + y_pomeraj) 
     prozor.blit(TextSurf, TextRect)
 def start_igre():
+    
     intro = True
-
+    
     while intro:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -30,22 +31,19 @@ def start_igre():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_s:
                     intro = False
+                    
                 if event.key == pygame.K_q:
                     pygame.quit()
                     quit()
-
-
         #prozor.fill((255,255,255))
         pozadina=pygame.image.load('start.jpg')
         prozor.blit(pozadina,(0,0))
         ispisi_poruku("Welcome to Scaffolder" , (0,155,0),-100,65)
         ispisi_poruku("Game by: " , (255,255,255),-70,25)
-        ispisi_poruku("Danilo||Alen||Bogosav" , (255,255,255),-50,25)
+        ispisi_poruku("Alen||Bogosav||Danilo" , (255,255,255),-50,25)
         ispisi_poruku("Klikni \"s\" za start ili \"q\" za quit!"  , (255,255,255),100,25)
         pygame.display.update()
         #pygame.time.Clock().tick(15)
-
-
 #animacija, treba popraviti, ovo je samo trenutno
 hodajDesno=[pygame.image.load('w1.png'),pygame.image.load('w2.png'),pygame.image.load('w3.png'),pygame.image.load('w4.png'),pygame.image.load('w5.png'),pygame.image.load('w6.png'),pygame.image.load('w7.png'),pygame.image.load('w8.png')]
 hodajLevo=[pygame.transform.flip(pygame.image.load('w1.png'),True,False),pygame.transform.flip(pygame.image.load('w2.png'),True,False),pygame.transform.flip(pygame.image.load('w3.png'),True,False),pygame.transform.flip(pygame.image.load('w4.png'),True,False),pygame.transform.flip(pygame.image.load('w5.png'),True,False),pygame.transform.flip(pygame.image.load('w6.png'),True,False),pygame.transform.flip(pygame.image.load('w7.png'),True,False),pygame.transform.flip(pygame.image.load('w8.png'),True,False)]
@@ -71,8 +69,6 @@ class player(object):
         self.hodanjeBrojac=0 #sluzi ako nasa animacija hodanja ima vise slika-faza hodanja
         self.isStoji = True
         self.platformaa=platformaa
-
-
     def draw(self,prozor):
         self.hitbox=(self.x+4,self.y,self.sirObjekta-24,self.visObjekta-10)#AAA
         if self.hodanjeBrojac + 1 >=9:
@@ -96,7 +92,6 @@ class platforma(object):
         self.y=y
         self.sirObjekta=sirObjekta
         self.visObjekta=20
-
     def draw(self, prozor):
         prozor.blit(pygame.transform.scale(platformaSlika,(self.sirObjekta,50)),(self.x,self.y))
         self.hitbox=(self.x+10,self.y+6,self.sirObjekta-20,30)
@@ -106,9 +101,6 @@ class platforma(object):
             if rect[1]+rect[3]> self.hitbox[1]:
                 return True
             return False
-            
-
-    
 def osveziSliku():
     # kad stavimo global to znaci da koristimo
     #vec postojecu promenljivu sa ovim imenom, deklarisanu van funkcije
@@ -121,25 +113,29 @@ def osveziSliku():
     pygame.display.update()
 
 
-zemlja = platforma(0,visinaProzora-37,sirinaProzora)
-igrac  = player(150,visinaProzora-108,42,78,zemlja)
 
-    
-brojacZaPadanje=0
-platforme = []
-platforme.append(zemlja)
-#GLAVNA UPDATE FUNKCIJA
+
+
+
+
 run=True
 GameOver = False
-start_igre()
+brojac=0
+#GLAVNA UPDATE FUNKCIJA
 while run:
+    if brojac==0:
+        brojac+=1
+        start_igre()
+        zemlja = platforma(0,visinaProzora-37,sirinaProzora)
+        igrac  = player(150,visinaProzora-108,42,78,zemlja)
+        brojacZaPadanje=0
+        platforme = []
+        platforme.append(zemlja)
     #milisekunde, FPS, koliko cesto se slika apdejta
     #for plat in platforme:
     #    if plat.collide(igrac.hitbox):
     #        igrac.y=plat.y+igrac.visObjekta
-
-    pygame.time.delay(27) #3*3
-    
+    pygame.time.delay(27) #3*3    
     while GameOver == True:
         pomocna_pozadina=pygame.image.load('start.jpg')
         prozor.blit(pomocna_pozadina,(0,0))
@@ -151,15 +147,19 @@ while run:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_s:
                     GameOver = False
                     run = True
+                    zemlja = platforma(0,visinaProzora-37,sirinaProzora)
+                    igrac  = player(150,visinaProzora-108,42,78,zemlja)
+                    brojacZaPadanje=0
+                    platforme = []
+                    platforme.append(zemlja)
+                    
                 if event.key == pygame.K_q:
                     GameOver = False
                     run = False
-
     if igrac.y <0:
         GameOver = True
         igrac.y = 300
@@ -180,44 +180,41 @@ while run:
                 platformaA = platforma(random.randint(platformaB.x,platformaB.x+100),platformaB.y-100, 200-random.randint(1,50))                
             platforme.append(platformaB)
             platforme.append(platformaA)
-
-
-
     keys=pygame.key.get_pressed()
-    #kretnja i granica kretnje
+    #KRETNJA I GRANICA KRETNJE
     if keys[pygame.K_LEFT] and igrac.x>0:
         igrac.x-=igrac.brzina
         igrac.levoOkrenut=True
         igrac.desnoOkrenut=False
-        if igrac.brzina < 20:
-            igrac.brzina+=1
+        slikaIgraca=pygame.transform.flip(pygame.image.load('w1.png'),True,False)
+        #if igrac.brzina < 20:
+        #    igrac.brzina+=1
     elif keys[pygame.K_RIGHT] and igrac.x<(sirinaProzora-igrac.sirObjekta):
         igrac.x+=igrac.brzina
         igrac.levoOkrenut=False
         igrac.desnoOkrenut=True
-        if igrac.brzina < 20:
-            igrac.brzina+=1
+        slikaIgraca=pygame.image.load('w1.png')
+        #if igrac.brzina < 20:
+        #    igrac.brzina+=1
     else:
         igrac.levoOkrenut=False
         igrac.desnoOkrenut=False
         igrac.hodanjeBrojac=0
-        igrac.brzina=5
-    #Skakanje
-    if not(igrac.isStoji):
+        #igrac.brzina=5
+    #SKAKANJE I PADANJE
+    if not(igrac.isStoji): #KAD NE STOJI
         for plat in platforme:
             if igrac.y+72>plat.y and igrac.y+72< plat.y+8 and igrac.x + igrac.sirObjekta-5>=plat.x+2 and igrac.x<= plat.x+plat.sirObjekta:
-                igrac.y=plat.y-igrac.visObjekta+7
+                igrac.y=plat.y-igrac.visObjekta+11
                 igrac.isStoji=True
                 igrac.platformaa=plat
                 brojacZaPadanje=0
                 break
-    else:
-        if igrac.x + igrac.sirObjekta-5<igrac.platformaa.x+2 or igrac.x> igrac.platformaa.x+igrac.platformaa.sirObjekta:
+    else: #KAD STOJI
+        if igrac.x + igrac.sirObjekta-10<igrac.platformaa.x+2 or igrac.x+15> igrac.platformaa.x+igrac.platformaa.sirObjekta:
             igrac.isStoji=False
-            
-    
-    if not(igrac.isSkok):
-        if not(igrac.isStoji):
+    if not(igrac.isSkok): 
+        if not(igrac.isStoji): #KAD PADA
             #if brojacZaPadanje<15:
             #    brojacZaPadanje+=1
             #igrac.y+=brojacZaPadanje*1.5
